3841525ef3c5f0d55f37245e7e3b7051f8d3a8f8
Rafael Petronilio <rafaelpetronilio@gmail.com>
2019-10-23 21:47:12 -0300

    steps

--- fiap-cucumber/src/test/java/br/com/fiap/Stepdefs.java
+++ fiap-cucumber/src/test/java/br/com/fiap/Stepdefs.java
@@ -1,8 +1,12 @@
 package br.com.fiap;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+// import cucumber.api.java.en.Given;
+// import cucumber.api.java.en.Then;
+// import cucumber.api.java.en.When;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
 
 import static org.junit.Assert.*;
 
